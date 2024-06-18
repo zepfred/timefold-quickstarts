a792e8fd202b8f27ad7586729c6317be558b9aa3
Christopher Chianelli <christopher@timefold.ai>
2024-06-18 12:05:34 -0400

    chore: rename timefold solver python package

--- .github/scripts/change_versions.sh
+++ .github/scripts/change_versions.sh
@@ -14,4 +14,4 @@ echo "New Python version: $NEW_PYTHON_VERSION"
 # Replaces the old version by the new version.
 find . -name pom.xml | xargs sed -i "s/>$OLD_JAVA_VERSION</>$NEW_JAVA_VERSION</g"
 find . -name build.gradle | xargs sed -i "s/\"$OLD_JAVA_VERSION\"/\"$NEW_JAVA_VERSION\"/g"
-find . -name pyproject.toml | xargs sed -i "s/timefold-solver == $OLD_PYTHON_VERSION/timefold-solver == $NEW_PYTHON_VERSION/g"
+find . -name pyproject.toml | xargs sed -i "s/timefold == $OLD_PYTHON_VERSION/timefold == $NEW_PYTHON_VERSION/g"
