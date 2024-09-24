bf33f5c97865f43554df499a559899987b48b51f
Fred <zepfred@gmail.com>
2024-09-24 09:56:51 -0300

    fix: fix partitioner

--- java/school-timetabling/src/main/java/org/acme/schooltimetabling/solver/SolverSolutionPartitioner.java
+++ java/school-timetabling/src/main/java/org/acme/schooltimetabling/solver/SolverSolutionPartitioner.java
@@ -4,12 +4,18 @@ import java.util.List;
 
 import ai.timefold.solver.core.api.score.director.ScoreDirector;
 import ai.timefold.solver.core.impl.partitionedsearch.partitioner.SolutionPartitioner;
+import ai.timefold.solver.core.impl.score.director.InnerScoreDirector;
 
 import org.acme.schooltimetabling.domain.Timetable;
 
 public class SolverSolutionPartitioner implements SolutionPartitioner<Timetable> {
     @Override
     public List<Timetable> splitWorkingSolution(ScoreDirector<Timetable> scoreDirector, Integer integer) {
-        return List.of(scoreDirector.getWorkingSolution());
+        Timetable timetable = ((InnerScoreDirector<Timetable, ?>) scoreDirector).cloneWorkingSolution();
+        timetable.getLessons().forEach(lesson -> {
+            lesson.setRoom(null);
+            lesson.setTimeslot(null);
+        });
+        return List.of(timetable);
     }
 }
